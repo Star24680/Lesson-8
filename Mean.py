8bce9b807f446adfc393f2a4cf6a7b4d3db58e02
Star24680 <gulerumaan1@gmail.com>
2024-08-28 18:02:08 +0500

    Commit

---
+++ Mean.py
@@ -0,0 +1,9 @@
+Mean=38
+Wrong_no=36
+Correct_no=56
+Total_no=40
+Sum=Mean*Total_no
+print("Sum of numbers is",Sum)
+NewNumber=Sum-(Wrong_no-Correct_no)
+NewMean=NewNumber/Total_no
+print("New Mean is=",NewMean)
